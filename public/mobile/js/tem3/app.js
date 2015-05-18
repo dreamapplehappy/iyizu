d9ba8afefd66180e3138ccae12de6b937c608136
dreamapplehappy <2451731631@qq.com>
2015-05-18 20:34:25 +0800

    add

---
+++ public/mobile/js/tem3/app.js
@@ -0,0 +1,30 @@
+angular.module('starter', ['ionic'])
+.controller("MyController", function($scope, $ionicActionSheet, $timeout) {
+
+ // Triggered on a button click, or some other target
+ $scope.show = function() {
+
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: '<b>Share</b> This' },
+       { text: 'Move' }
+     ],
+     destructiveText: 'Delete',
+     titleText: 'Modify your album',
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
+
+   // For example's sake, hide the sheet after two seconds
+   $timeout(function() {
+     hideSheet();
+   }, 2000);
+
+ };
+});
