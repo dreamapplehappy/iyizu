95d82313d609a32e84e14d77bd8ebe1315a4e114
dreamapplehappy <2451731631@qq.com>
2015-05-20 13:46:39 +0800

    oo

---
+++ public/mobile/js/tem5/app.js
@@ -0,0 +1,18 @@
+angular.module('starter', ['ionic'])
+.controller("MyController", function($scope) {
+    $scope.lists = [
+    	{	status: "new", content: "I need to do1"},
+    	{	status: "new", content: "I need to do2"},
+    	{	status: "new", content: "I need to do3"},
+    	{	status: "new", content: "I need to do4"}
+    ];
+    $scope.add = function(){
+    	var todo = {};
+    	todo.status = "new";
+    	todo.content = $scope.thing;
+    	$scope.lists.push(todo);
+    }
+    $scope.remove = function(){
+
+    }
+});
