3e154e1e8fb03b4305fef447f815be519637d0cf
dreamapplehappy <2451731631@qq.com>
2015-05-27 13:45:57 +0800

    o

--- public/angularjs/12/route.js
+++ public/angularjs/12/route.js
@@ -7,6 +7,26 @@ angular.module("MyApp", ["ngRoute"])
 	});
 	
 }])
+.factory("githubService", ["$http", function($http){
+	var githubUrl = "http://api.github.com";
+	var githubUsername;
+
+	var runUserRequest = function(path){
+		return $http({
+			method: "JSONP",
+			url: githubUrl + "/users/" + githubUsername + "/" + path + "?callback=JSON_CALLBACK"
+		});
+	}
+
+	return {
+		events: function(){
+			return runUserRequest("events");
+		},
+		setUsername: function(username){
+			githubUsername = username;
+		}
+	};
+}])
 .controller("IndexController", ["$scope", function($scope){
 	$scope.name = "dreamapple";
 }]);
