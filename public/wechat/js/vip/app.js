fbfa71a7dc1c23d371765ff3ec58f6fe5079a6da
dreamapplehappy <2451731631@qq.com>
2015-05-27 19:04:22 +0800

    c

--- public/wechat/js/vip/app.js
+++ public/wechat/js/vip/app.js
@@ -3,16 +3,20 @@ $(function(){
 	$("#card-container").click(function(){
 		if(0 == flag){
 			$("#card").removeClass("filp");
+			$("#front").removeClass("filpc");
 			$("#front").hide();
 			$("#back").show();
 			$("#card").addClass("filp");
+			$("#back").addClass("filpc");
 			flag = 1;
 		}
 		else if(1 == flag){
+			$("#back").removeClass("filpc");
 			$("#card").removeClass("filp");
 			$("#back").hide();
 			$("#card").addClass("filp");
 			$("#front").show();
+			$("#front").addClass("filpc");
 			flag = 0;
 		}
 	});
