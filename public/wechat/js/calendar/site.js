c727e85a2bd9822c4ee379652459f625f9957da5
dreamapplehappy <2451731631@qq.com>
2015-05-31 20:31:30 +0800

    asd

--- public/wechat/js/calendar/site.js
+++ public/wechat/js/calendar/site.js
@@ -9,7 +9,7 @@ $(document).ready( function() {
 
   // here's some magic to make sure the dates are happening this month.
   var thisMonth = moment().format('YYYY-MM');
-  /*var data = ["2015-5-29","2015-5-30"];
+  var data = ["2015-5-27","2015-5-22", "2015-5-19"];
   var eventArray = [];
   for(var i = 0; i < data.length; i++){
     var obj = {
@@ -17,13 +17,13 @@ $(document).ready( function() {
       title: "sign"
     }
     eventArray.push(obj);
-  }*/
-  // console.log(eventArray);
-  var eventArray = [
+  }
+  console.log(eventArray);
+  /*var eventArray = [
     { date: thisMonth + '-27', title: 'Single Day Event' },
     { date: thisMonth + '-22', title: 'Single Day Event' },
     { date: thisMonth + '-19', title: 'Single Day Event' }
-  ];
+  ];*/
 
   // the order of the click handlers is predictable.
   // direct click action callbacks come first: click, nextMonth, previousMonth, nextYear, previousYear, or today.
